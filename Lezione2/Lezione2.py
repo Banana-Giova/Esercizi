84b600db78ceeb0dba08436eb24874d03fca60bf
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-04-18 12:24:56 +0200

    Risolto primo esercizio

---
+++ Lezione2/Lezione2.py
@@ -0,0 +1,28 @@
+#Giovanni di Giuseppe   18/04/2024
+
+while False:
+    print("I gonfaloni")
+
+"""
+2-3. Personal Message: 
+- Use a variable to represent a person’s name, and print a message to that person. 
+Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+2-4. Name Cases: 
+- Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case.
+2-5. Famous Quote: 
+- Find a quote from a famous person you admire. Print the quote and the name of its author. 
+Your output should look something like the following, including the quotation marks: 
+Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+2-6. Famous Quote 2: 
+- Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. 
+Then compose your message and represent it with a new variable called message. Print your message. 
+2-8. File Extensions: 
+- Python has a removesuffix() method that works exactly like removeprefix(). 
+Assign the value 'python_notes.txt' to a variable called filename. 
+Then use the removesuffix() method to display the filename without the file extension, like some file browsers do.
+"""
+
+#2-3. Personal Message
+nome:str = "Mario"        #variabile del nome
+print(f"{nome} s'annamo a pija' un gelato?")        #print del messaggio
+
