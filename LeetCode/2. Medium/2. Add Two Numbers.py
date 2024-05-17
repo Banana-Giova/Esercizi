95a7b3743a85a073683b3aaad33216ffd2264d6c
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-05-17 08:59:10 +0200

    LeetCode updated

---
+++ LeetCode/2. Medium/2. Add Two Numbers.py
@@ -0,0 +1,25 @@
+class Solution:
+    def addTwoNumbers(self, l1: list, 
+                      l2: list) -> list[int]:
+        
+        #Not working because ListNodes are trash
+        #Logically it does indeed work
+
+        list(str(l1))
+        list(str(l2))
+        l1.reverse()
+        l2.reverse()
+
+        l1_str:str = ''
+        for i in l1:
+            l1_str += str(i)
+        l2_str:str = ''
+        for i in l2:
+            l2_str += str(i)
+        
+        l3 = list(str(int(l1_str) + int(l2_str)))
+        l3.reverse()
+        return l3
+    
+solution:Solution = Solution()
+print(solution.addTwoNumbers([2,4,3], [5,6,4]))
