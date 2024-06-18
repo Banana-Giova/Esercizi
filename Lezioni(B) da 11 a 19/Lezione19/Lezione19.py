ba69b2543e7ce315b399c2469fdd9fa1e6ec828f
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-06-18 13:57:55 +0200

    Lezione 19 avanzata

---
+++ Lezioni(B) da 11 a 19/Lezione19/Lezione19.py
@@ -0,0 +1,20 @@
+def mergeSort(listing:list[int] | list[float]) -> list:
+
+    midpoint:int = len(listing)//2
+
+    if len(listing) > 1:
+        first_half = mergeSort(listing=listing[:midpoint])
+        second_half = mergeSort(listing=listing[midpoint:])
+        new_listing = []
+        fi, si = 0, 0
+        for i in range(len(first_half + second_half)):
+            if first_half[fi] < second_half[si]:
+                new_listing.append(first_half[fi])
+                fi += 1
+            else:
+                new_listing.append(second_half[si])
+                si += 1
+    else:
+        return listing
+    
+mergeSort([3,2,4,5,2,1])
