605b0fd2f6aad0ad14855c6296717a2c285754b4
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-06-27 13:55:41 +0200

    Lezione 20 avanzata

--- Python/Lezioni(B) da 11 a 20/Lezione20/Lezione20.py
+++ Python/Lezioni(B) da 11 a 20/Lezione20/Lezione20.py
@@ -74,12 +74,100 @@ class PagamentoContanti(Pagamento):
         super().__init__()
 
         self.importo = self.setImporto(importo)
-
-    def dettagliPagamento(self) -> str:
-        return super().dettagliPagamento() + "effettuato con contanti"
     
     def inPezziDa(self):
-        pass
+        if self.importo == 0:
+            return "Non c'è nulla da pagare!"
+        else:
+            banconote:dict = {
+                500:0,
+                200:0,
+                100:0,
+                50:0,
+                20:0,
+                10:0,
+                5:0
+            }
+            monete:dict = {
+                2:0,
+                1:0,
+                0.5:0,
+                0.2:0,
+                0.1:0,
+                0.05:0,
+                0.02:0,
+                0.01:0
+            }
+            totale_parziale:float = self.importo
+
+            if True:
+                while totale_parziale >= 500:
+                    totale_parziale -= 500
+                    banconote[500] += 1
+                while totale_parziale >= 200:
+                    totale_parziale -= 200
+                    banconote[200] += 1
+                while totale_parziale >= 100:
+                    totale_parziale -= 100
+                    banconote[100] += 1
+                while totale_parziale >= 50:
+                    totale_parziale -= 50
+                    banconote[50] += 1
+                while totale_parziale >= 20:
+                    totale_parziale -= 20
+                    banconote[20] += 1
+                while totale_parziale >= 10:
+                    totale_parziale -= 10
+                    banconote[10] += 1
+                while totale_parziale >= 5:
+                    totale_parziale -= 5
+                    banconote[5] += 1
+                while totale_parziale >= 2:
+                    totale_parziale -= 2
+                    monete[2] += 1
+                while totale_parziale >= 1:
+                    totale_parziale -= 1
+                    monete[1] += 1
+                while totale_parziale >= 0.5:
+                    totale_parziale -= 0.5
+                    monete[0.5] += 1
+                while totale_parziale >= 0.2:
+                    totale_parziale -= 0.2
+                    monete[0.2] += 1
+                while totale_parziale >= 0.1:
+                    totale_parziale -= 0.1
+                    monete[0.1] += 1
+                while totale_parziale >= 0.05:
+                    totale_parziale -= 0.05
+                    monete[0.05] += 1
+                while totale_parziale >= 0.02:
+                    totale_parziale -= 0.02
+                    monete[0.02] += 1
+                while totale_parziale >= 0.01:
+                    totale_parziale -= 0.01
+                    monete[0.01] += 1
+            
+            output = f'{self.importo} euro da pagare in contanti con:\n'
+            for ki, vi in banconote.items():
+                if vi != 0:
+                    if vi == 1:
+                        sing_plur:str = 'a'
+                    else:
+                        sing_plur:str = 'e'
+                    output += f"{vi} banconot{sing_plur} da {ki} euro\n"
+            for ki, vi in monete.items():
+                if vi != 0:
+                    if vi == 1:
+                        sing_plur:str = 'a'
+                    else:
+                        sing_plur:str = 'e'
+                    output += f"{vi} monet{sing_plur} da {ki} euro\n"
+        return output
+    
+    def dettagliPagamento(self) -> str:
+        return super().dettagliPagamento() + "effettuato con contanti\n" + self.inPezziDa()
+
+                
 
 class PagamentoCartaDiCredito(Pagamento):
     def __init__(self, importo:float, titolare_carta:str, 
@@ -87,6 +175,85 @@ class PagamentoCartaDiCredito(Pagamento):
         super().__init__()
 
         self.importo = self.setImporto(importo)
+        self.titolare_carta = titolare_carta
+        self.data_di_scadenza = data_di_scadenza
+        self.numero_carta = numero_carta
 
     def dettagliPagamento(self) -> str:
-        return super().dettagliPagamento() + "effettuato con la carta di credito"
+        return super().dettagliPagamento() + "effettuato con la carta di credito\n"\
+        + f"Nome sulla carta: {self.titolare_carta}\nData di scadenza: {self.data_di_scadenza}\n"\
+        + f"Numero della carta: {self.numero_carta}"
+    
+
+
+
+"""
+2. RENDERING GRAFICO
+Si vuole sviluppare un sistema in Python per gestire il rendering di diverse 
+forme geometriche. Il sistema dovrà supportare almeno tre tipi di forme: 
+quadrati, rettangoli, e triangoli rettangoli.
+
+Definire la classe astratta Forma che sarà utilizzata per definire l'attributo 
+corrispondente al nome della forma e le funzionalità base di ogni forma, 
+come i metodi astratti getArea() per calcolare l'area e render() 
+per disegnare su schermo la forma.
+
+Definire la classe Quadrato che estende la classe Forma 
+e aggiunge specifiche circa la lunghezza di un suo lato.
+Il costruttore della classe deve ricevere come argomento solo il lato del quadrato, 
+ed impostare il nome della forma su "Quadrato".
+Il metodo getArea() deve calcolare l'area del quadrato.
+Il metodo render() deve stamapre su schermo un quadrato avente lato 
+pari al valore passato nel costruttore. 
+Il quadrato da stampare deve essere un quadrato vuoto (" "), 
+avente degli asterischi ("*") lungo il suo perimetro. (Vedi Esempio di output)
+
+Definire la classe Rettangolo che estende la classe Forma 
+e aggiunge specifiche circa la lunghezza della sua base e della sua altezza.
+Il costruttore della classe deve ricevere come argomento solo la base 
+e l'altezza del rettangolo, ed impostare il nome della forma su "Rettangolo".
+Il metodo getArea() deve calcolare l'area del rettangolo.
+Il metodo render() deve stampare su schermo un rettangolo avente base 
+ed altezza pari ai valori passati nel costruttore. 
+Il rettangolo da stampare deve essere un rettangolo vuoto (" "), 
+avente degli asterischi ("*") lungo il suo perimetro. (Vedi Esempio di output)
+
+Definire la classe Triangolo che estende la classe Forma 
+e aggiunge specifiche circa la dimensione di un lato del triangolo (per semplicità, 
+si suppone che il triangolo in questione sia un triangolo rettangolo).
+Il costruttore della classe deve ricevere come argomento solo il lato del triangolo, 
+ed impostare il nome della forma su "Triangolo".
+Il metodo getArea() deve calcolare l'area del triangolo.
+Il metodo render() deve stampare su schermo un triangolo rettangolo 
+avente i due cateti di lunghezza pari ai valori passati nel costruttore. 
+Il triangolo da stampare deve essere un triangolo vuoto (" "), 
+avente degli asterischi ("*") lungo il suo perimetro. (Vedi Esempio di output)
+ 
+Hint: per il disegno utilizzare print("*", end=" "), 
+dato che l'argomento end = " " permette di controllare 
+come termina ogni chiamata a print, e impostandolo a uno spazio si può fare in modo 
+che tutte le stampe successive siano sulla stessa riga, separate da uno spazio.
+
+Esempi di output:
+Ecco un Quadrato di lato 4!
+
+* * * *
+*     *
+*     *
+* * * *
+L'area di questo quadrato vale: 16
+
+Ecco un Rettangolo avente base 8 ed altezza 4!
+* * * * * * * *
+*             *
+*             *
+* * * * * * * *
+L'area di questo rettangolo vale: 32
+
+Ecco un Triangolo avente base 4 ed altezza 4!
+*      
+* *    
+*   *  
+* * * *
+L'area di questo triangolo vale: 8.0
+"""
