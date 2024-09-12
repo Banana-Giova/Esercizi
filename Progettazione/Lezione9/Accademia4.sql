b2e83a5eece5bd2e151176ab838b0e732ccf7416
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-09-12 17:25:32 +0200

    Lezione 9 completata

--- Progettazione/Lezione9/Accademia4.sql
+++ Progettazione/Lezione9/Accademia4.sql
@@ -1,23 +1,33 @@
---Query 1--
+--Query 1 -> Giusta, con appunto--
 SELECT distinct cognome 
 FROM persona;
+--WHERE TRUE;--
+--where è opzionale, ma è meglio metterlo
 
---Query 2--
+--Query 2 -> Giusta, ma da indentare--
 SELECT id, nome, cognome 
 FROM persona 
 WHERE posizione = 'Ricercatore';
 
---Query 3--
+--Query 3 -> Giusta, ma da indentare--
 SELECT id, cognome 
 FROM persona 
 WHERE posizione = 'Professore Associato' and cognome like 'V%';
 
---Query 4--
+--Query 4 -> Sbagliata--
 SELECT id, cognome 
 FROM persona 
 WHERE posizione = 'Professore Ordinario' or posizione = 'Professore Associato' and cognome like 'V%';
+--WHERE ( posizione = 'Professore Ordinario' 
+    --or posizione = 'Professore Associato' )
+    --and cognome like 'V%';
 
---Query 5--
+--oppure--
+
+--WHERE posizione IN ('Professore Ordinario', 'Professore Associato')
+    --and cognome like 'V%';
+
+--Query 5 -> Giusta, ricorda le maiuscole per ortografia--
 select id, nome
 from Progetto
 where CURRENT_DATE > fine;
@@ -32,15 +42,15 @@ SELECT nome
 FROM wp
 ORDER BY nome ASC;
 
---Query 8--
+--Query 8 -> Giusta, ma ricorda il WHERE--
 SELECT distinct tipo
 FROM assenza;
 
---Query 9--
+--Query 9 -> Giusta, ma ricorda il WHERE--
 SELECT distinct tipo
 FROM attivitaprogetto;
 
---Query 10--
+--Query 10 -> Giusta--
 SELECT distinct giorno
 FROM attivitanonprogettuale
 WHERE tipo = 'Didattica'
