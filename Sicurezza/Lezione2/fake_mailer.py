45e1a30f0919e9ab48cdd1efcc50af69e1d2e8cc
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-09-12 17:25:20 +0200

    Lezione 2 avanzata

--- Sicurezza/Lezione2/fake_mailer.py
+++ Sicurezza/Lezione2/fake_mailer.py
@@ -6,33 +6,32 @@ from email.utils import formataddr
 
 def send_email(real_email, fake_sender_email, header, receiver_email, receiver_name, subject, body, password):
     # Set up the SMTP server
-    smtp_server = "smtp.gmail.com"
+    smtp_server = "smtp.libero.it"
     smtp_port = 587
 
     # Create the email
-    """
+
     msg = MIMEMultipart('alternative')
     msg['From'] = formataddr((str(Header(header, 'utf-8')), fake_sender_email))
     msg['To'] = receiver_email
     msg['Subject'] = subject
     msg.attach(MIMEText(body, 'plain'))
-    """
-    msg = f"From: {header} <{fake_sender_email}>\nTo: {receiver_name} <{receiver_email}>\nSubject: {subject}\n\n{body}>"
+    
     try:
         # Connect to the server and send the email
         server = smtplib.SMTP(smtp_server, smtp_port)
         server.starttls()  # Secure the connection
         server.login(real_email, password)
         #text = msg.as_string()
-        server.sendmail(real_email, receiver_email, msg.encode())
+        server.sendmail(real_email, receiver_email, msg.as_string())
         server.quit()
         print("Email sent successfully!")
     except Exception as e:
         print(f"Failed to send email. Error: {e}")
 
 # Example usage
-real_email = "giovannidgad@gmail.com"
-password = "gdjj ictp guxi mgpf"
+real_email = "banana_giova@libero.it"
+password = "ReptarStriker32!"
 fake_sender_email = "principe-nigeriano@not-a-scam.com"
 receiver_name = "Giovanni"
 receiver_email = "giovannidigiuseppe@protonmail.com"
