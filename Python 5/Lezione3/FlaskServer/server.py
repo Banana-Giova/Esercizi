95437813ad1253ab57cda6cde6aa328352395718
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-09-17 13:48:11 +0200

    Lezione 3 completata

--- Python 5/Lezione3/FlaskServer/server.py
+++ Python 5/Lezione3/FlaskServer/server.py
@@ -1,13 +1,7 @@
 from flask import Flask, render_template, request
+import json
 api = Flask(__name__, static_url_path='/static')
 
-vittime:list[tuple[str]] = [
-    ("Michelone", "Badasium"),
-    ("Danielone", "Pietrosmusi"),
-    ("Romeo", "Sgarrapisi"),
-    ("Samuele", "Sgarrapisi"),
-    ("Cristiano", "Ronaldo")
-]
 
 #main route
 @api.route('/', methods=['GET'])
@@ -36,14 +30,30 @@ def ronaldo():
 #registration
 @api.route('/login', methods=['GET'])
 def login():
+    with open('data/vittime.json') as f:
+        vittime:dict = json.load(f)
     nome = request.args.get("Nome")
     print(f"Nome selezionato: {nome}")
     cognome = request.args.get("Cognome")
     print(f"Cognome selezionato: {cognome}")
-    for i in vittime:
-        if i[0] == nome and i[1] == cognome:
-            return render_template('regko.html')
-    return render_template('regok.html')
+    paprika = request.args.get("Paprika")
+    print(f"Patatine {paprika} paprika")
+    context = {
+        "nome":nome, 
+        "cognome":cognome,
+        "paprika":paprika
+        }
+    for ki, vi in vittime.items():
+    #if already a user
+        last_pk:int = int(ki)
+        if vi[0] == nome and vi[1] == cognome and vi[2] == paprika:
+            return render_template('regko.html', **context)
+    
+    #if not a user
+    vittime[str(last_pk+1)] = [nome, cognome, paprika]
+    with open('data/vittime.json', mode='w', encoding='utf-8') as f:
+        f.write(json.dumps(vittime, indent=True))
+    return render_template('regok.html', **context)
 
 
 #api run segment
