3166dc0fd91f7b2017f3baddba60df356f2f7944
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-09-21 18:16:21 +0200

    Lezione 3 completata

---
+++ Python 5/Lezione3/FlaskServer/models.py
@@ -0,0 +1,5 @@
+class Preghiera:
+    def __init__(self, titolo:str, testo:str, data:int) -> None:
+        self.titolo = titolo
+        self.testo = testo
+        self.data = data
--- Python 5/Lezione3/FlaskServer/server.py
+++ Python 5/Lezione3/FlaskServer/server.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import json
+from models import Preghiera
 import os.path
-import datetime
+from datetime import date
 api = Flask(__name__, static_url_path='/static')
 
 
@@ -33,16 +34,19 @@ def ronaldo():
 
 
 #registration
-@api.route('/login', methods=['GET'])
+@api.route('/login', methods=['POST'])
 def login():
     with open('data/vittime.json') as f:
         vittime:dict = json.load(f)
-    nome = request.args.get("Nome")
-    print(f"Nome selezionato: {nome}")
-    cognome = request.args.get("Cognome")
-    print(f"Cognome selezionato: {cognome}")
-    paprika = request.args.get("Paprika")
-    print(f"Patatine {paprika} paprika")
+    if request.method == "POST":
+        nome = request.form["Nome"]
+        print(f"Nome selezionato: {nome}")
+        cognome = request.form["Cognome"]
+        print(f"Cognome selezionato: {cognome}")
+        paprika = request.form["Paprika"]
+        print(f"Patatine {paprika} paprika")
+    else:
+        return render_template('vilgax.html')
     context = {
         "nome":nome, 
         "cognome":cognome,
@@ -52,6 +56,14 @@ def login():
     #if already a user
         last_pk:int = int(ki)
         if vi[0] == nome and vi[1] == cognome and vi[2] == paprika:
+            if paprika == 'senza':
+                papritext:str = 'S'
+            elif paprika == 'con':
+                papritext = 'C'
+            global current_user
+            current_user = nome + cognome + papritext
+            global current_day
+            current_day = int((str(date.today())).replace('-',''))
             return render_template('regko.html', **context)
     
     #if not a user
@@ -64,8 +76,28 @@ def login():
 #preghiere
 @api.route('/preghiere', methods=['GET'])
 def preghiere():
-    if os.path.exists('data/vittime.json'):
-        return render_template('preghiere.html')
+    if os.path.exists('data/preghiere.json'):
+        with open('data/preghiere.json', mode='r') as f:
+            preghiere:dict = json.load(f)
+        if current_user not in preghiere:
+            preghiere[current_user] = []
+        with open('data/preghiere.json', mode='w', encoding='utf-8') as f:
+            f.write(json.dumps(preghiere, indent=True))
+
+        current_preghiere:list[list[str, str, int]] = preghiere[current_user]
+        context = {
+            "da_esaudire": [],
+            "esaudite": []
+        }
+        for i in current_preghiere:
+            singola_preg = Preghiera(
+                titolo=i[0], testo=i[1], data=i[2]
+            )
+            if singola_preg.data >= current_day:
+                context["da_esaudire"].insert(0, singola_preg)
+            else:
+                context["esaudite"].insert(0, singola_preg)
+        return render_template('preghiere.html', **context)
     else:
         raise Exception('The file "preghiere.json" has to be present in "data/" for the server to function!')
 
@@ -73,10 +105,24 @@ def preghiere():
 def new_preghiera():
     if request.method == 'POST':
         titolo_preg = request.form['titolo_preg']
+        print(titolo_preg)
         testo_preg = request.form['testo_preg']
-        istante_preg = datetime.datetime.now()
+        print(testo_preg)
+        istante_preg = int((str(date.today())).replace('-',''))
+        print(istante_preg)
     else:
         return render_template('preghiere.html')
+    preghierina:list = [titolo_preg, testo_preg, istante_preg]
+    
+    with open('data/preghiere.json', mode='r', encoding='utf-8') as f:
+        preghiere:dict = json.load(f)
+    if current_user not in preghiere:
+        preghiere[current_user] = []
+    preghiere[current_user].append(preghierina)
+    with open('data/preghiere.json', mode='w', encoding='utf-8') as f:
+        f.write(json.dumps(preghiere, indent=True))
+    return redirect('/preghiere')
+
 
 
 @api.route('/lista_preghiere', methods=['GET'])
