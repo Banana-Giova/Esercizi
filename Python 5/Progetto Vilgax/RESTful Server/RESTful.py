eedae7684ba5f986e8feae18eaf2356943a056b7
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-10-01 19:02:52 +0200

    Progetto Vilgax avanzato

--- Python 5/Progetto Vilgax/RESTful Server/RESTful.py
+++ Python 5/Progetto Vilgax/RESTful Server/RESTful.py
@@ -1,64 +1,36 @@
-from flask import Flask, json, request, redirect # type: ignore
+from flask import Flask, json, request, redirect, abort, jsonify
 import os.path
 from datetime import date
 import json
-import ast
 
 api = Flask(__name__)
 
-valid_nums = [1,2,3,4,5,6,7]
-
 @api.route('/', methods=['GET', 'POST'])
-def restful_get():
-    if request.method == 'POST':
-        sOper = int(request.form['test'])
-    if sOper not in valid_nums:
-        raise Exception("Operazione non valida.")    
-    while sOper != 7:
-        if sOper == 1:
-            with open('data/vittime.json', mode='r') as f:
-                vittime:dict = json.load(f)
-                return vittime
-        elif sOper == 2:
-            with open('data/preghiere.json', mode='r') as f:
-                preghiere:dict = json.load(f)
-                return preghiere
-        elif sOper == 3:
-            with open('data/recensioni.json', mode='r') as f:
-                recensioni:dict = json.load(f)
-                return recensioni
-        else:
-            return redirect(f'/mod{sOper}')
-        
-@api.route('/mod4', methods=['GET'])
-def restful_mod4():
-    try:
-        vittime = ast.literal_eval(input("Fornire nuovo dizionario\n>>>"))
-        with open('data/vittime.json', mode='w', encoding='utf-8') as f:
-            f.write(json.dumps(vittime, indent=True))
-        return "Successo"
-    except:
-        return "Fallimento"
-
-@api.route('/mod5', methods=['GET'])
-def restful_mod5():
-    try:
-        preghiere = ast.literal_eval(input("Fornire nuovo dizionario\n>>>"))
-        with open('data/preghiere.json', mode='w', encoding='utf-8') as f:
-            f.write(json.dumps(preghiere, indent=True))
-        return "Successo"
-    except:
-        return "Fallimento"
-
-@api.route('/mod6', methods=['GET'])
-def restful_mod6():
-    try:
-        recensioni = ast.literal_eval(input("Fornire nuovo dizionario\n>>>"))
-        with open('data/recensioni.json', mode='w', encoding='utf-8') as f:
-            f.write(json.dumps(recensioni, indent=True))
-        return "Successo"
-    except:
-        return "Fallimento"
+def index():
+    sOper = request.json.get('test', 0)
+    match int(sOper):
+        case 1 | 4:
+            data_name = 'vittime'
+        case 2 | 5:
+            data_name = 'preghiere'
+        case 3 | 6:
+            data_name = 'recensioni'
+        case _:
+            abort(422)
+    match int(sOper):
+        case 1 | 2 | 3:
+            with open(f'data/{data_name}.json', mode='r') as f:
+                sendata:dict = json.load(f)
+                return jsonify(sendata)
+        case 4 | 5 | 6:
+            redata = request.json.get('context', 0)
+            with open(f'data/{data_name}.json', mode='a', encoding='utf-8') as f:
+                f.write(json.dumps(redata, indent=True))
+            with open(f'data/{data_name}.json', mode='r') as f:
+                sendata:dict = json.load(f)
+            return jsonify(sendata)
+        case _:
+            abort(422)
 
 if __name__ == '__main__'and\
 os.path.exists('data/vittime.json') and\
--- Python 5/Progetto Vilgax/TestServer/test_server.py
+++ Python 5/Progetto Vilgax/TestServer/test_server.py
@@ -1,18 +1,24 @@
 from flask import Flask, json, redirect # type: ignore
 import json
-import socket, time
 import requests
 
 api = Flask(__name__)
 
-context = {
-    'test':1
-}
-
 @api.route('/', methods=['GET'])
 def testing():
-    response = requests.post("http://127.0.0.1:1240", data=context)
-    return 
+    data = { 'test': 4 }
+    addict:dict = {"1": 
+                  ["Pippo", "Baudo", "senza"]}
+    
+    url = 'http://127.0.0.1:1240/'
+    headers = { 'Content-Type': 'application/json' }
+    response = requests.post(url, data=json.dumps(data), headers=headers,
+                             json=addict)
+    response.raise_for_status()
+    data = response.json()
+    
+    return data
+
 
 if __name__ == '__main__':
     api.run(host="127.0.0.1", port=2480)
