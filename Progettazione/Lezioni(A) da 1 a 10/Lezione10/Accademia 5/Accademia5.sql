98e2f3bd4d9025e17640e4a35498844734380218
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-10-04 19:18:43 +0200

    Lezione 10 avanzata

--- Progettazione/Lezioni(A) da 1 a 10/Lezione10/Accademia 5/Accademia5.sql
+++ Progettazione/Lezioni(A) da 1 a 10/Lezione10/Accademia 5/Accademia5.sql
@@ -1,17 +1,27 @@
 --1. Quali sono il nome, la data di inizio e 
 --la data di fine dei WP del progetto di nome ‘Pegasus’?
 
-
-
-
+SELECT wp.nome AS nome,
+       wp.inizio AS data_di_inizio,
+       wp.fine AS data_di_fine
+FROM WP wp, Progetto prog
+WHERE prog.id = wp.progetto
+  AND prog.nome = 'Pegasus';
 
 --2. Quali sono il nome, il cognome e la posizione degli 
 --strutturati che hanno almeno una attività nel progetto ‘Pegasus’, 
 --ordinati per cognome decrescente?
 
-
-
-
+SELECT DISTINCT pers.nome AS nome_strutturato,
+       pers.cognome AS cognome_strutturato,
+       pers.posizione AS posizione
+FROM Persona pers,
+     AttivitaProgetto attprog,
+     Progetto prog
+WHERE attprog.persona = pers.id
+  AND attprog.progetto = prog.id
+  AND prog.nome = 'Pegasus'
+ORDER BY cognome_strutturato DESC;
 
 --3. Quali sono il nome, il cognome e la posizione degli 
 --strutturati che hanno più di una attività nel progetto ‘Pegasus’?
