b74f9eaa90cc19828118a785711c5760cd7b853a
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-10-16 13:43:40 +0200

    Lezione 11 avanzata

--- Progettazione/Lezioni(B) da 11 a 20/Lezione11/Accademia6.sql
+++ Progettazione/Lezioni(B) da 11 a 20/Lezione11/Accademia6.sql
@@ -23,37 +23,97 @@ WHERE prog.budget >= 50000
 --4. Qual è la media, il massimo e il minimo delle ore 
 --delle attività relative al progetto ‘Pegasus’ ?
 
-
+SELECT AVG(attprog.oreDurata) AS media_ore,
+       MAX(attprog.oreDurata) AS massimo_ore,
+       MIN(attprog.oreDurata) AS minimo_ore
+FROM AttivitaProgetto attprog
+WHERE TRUE;
 
 --5. Quali sono le medie, i massimi e i minimi delle ore 
 --giornaliere dedicate al progetto ‘Pegasus’ da ogni singolo docente?
 
-
+SELECT pers.nome AS nome_docente,
+       pers.cognome AS cognome_docente,
+       AVG(attprog.oreDurata) AS media_ore,
+       MAX(attprog.oreDurata) AS massimo_ore,
+       MIN(attprog.oreDurata) AS minimo_ore
+FROM AttivitaProgetto attprog
+    JOIN Persona pers
+        ON attprog.persona = pers.id
+WHERE TRUE
+GROUP BY pers.id;
 
 --6. Qual è il numero totale di ore dedicate alla 
 --didattica da ogni docente?
 
-
+SELECT pers.nome AS nome_docente,
+       pers.cognome AS cognome_docente,
+       SUM(attprog.oreDurata) AS totale_ore
+FROM AttivitaProgetto attprog
+    JOIN Persona pers
+        ON attprog.persona = pers.id
+WHERE TRUE
+GROUP BY pers.id;
 
 --7. Qual è la media, il massimo e il minimo degli 
 --stipendi dei ricercatori?
 
-
+SELECT AVG(pers.stipendio) AS media_stipendio,
+       MAX(pers.stipendio) AS massimo_stipendio,
+       MIN(pers.stipendio) AS minimo_stipendio
+FROM Persona pers
+WHERE pers.posizione = 'Ricercatore';
 
 --8. Quali sono le medie, i massimi e i minimi degli stipendi 
 --dei ricercatori, dei professori associati e dei professori ordinari?
 
-
+SELECT pers.posizione AS posizione,
+       AVG(pers.stipendio) AS media_stipendio,
+       MAX(pers.stipendio) AS massimo_stipendio,
+       MIN(pers.stipendio) AS minimo_stipendio
+FROM Persona pers
+WHERE TRUE
+GROUP BY pers.poszione;
 
 --9. Quante ore ‘Ginevra Riva’ ha dedicato ad ogni progetto 
 --nel quale ha lavorato?
 
-
+SELECT prog.nome AS progetto,
+       SUM(attprog.oreDurata) AS ore_dedicate
+FROM AttivitaProgetto attprog
+    JOIN Persona pers
+        ON attprog.persona = pers.id
+    JOIN Progetto prog
+        ON attprog.progetto = prog.id
+WHERE pers.nome = 'Ginevra'
+  AND pers.cognome = 'Riva'
+GROUP BY prog.id;
 
 --10. Qual è il nome dei progetti su cui lavorano 
 --più di due strutturati?
 
-
+SELECT prog.nome AS nome_progetto,
+       COUNT(attprog.persona) AS numero_di_strutturati
+FROM AttivitaProgetto attprog
+    JOIN Persona pers
+        ON attprog.persona = pers.id
+    JOIN Progetto prog
+        ON attprog.progetto = prog.id
+WHERE TRUE
+GROUP BY prog.id
+HAVING COUNT(attprog.persona) >= 2;
 
 --11. Quali sono i professori associati che hanno lavorato 
 --su più di un progetto?
+
+SELECT pers.nome AS nome_docente,
+       pers.cognome AS cognome_docente,
+       COUNT(attprog.persona) AS numero_di_progetti
+FROM AttivitaProgetto attprog
+    JOIN Persona pers
+        ON attprog.persona = pers.id
+    JOIN Progetto prog
+        ON attprog.progetto = prog.id
+WHERE TRUE
+GROUP BY pers.id
+HAVING COUNT(attprog.persona) >= 2;
