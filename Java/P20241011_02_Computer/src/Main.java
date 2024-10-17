94756533b53577779d65807ecb3d469233e5cdd4
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-10-17 21:37:31 +0200

    Progetto Java completato

--- Java/P20241011_02_Computer/src/Main.java
+++ Java/P20241011_02_Computer/src/Main.java
@@ -3,6 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		
 		Computer comp = new Computer(500, 10, 24, 12, 8, "Lenovo", 2012);
 		
 		
@@ -12,7 +15,13 @@ public class Main {
 		"Produttore: " + comp.getProduttore() + "\n" +
 		"Anno di Produzione: " + comp.getAnno_produzione()
 		);
-
+		
+		System.out.println("Numero di PC al momento: " + comp.getPCnum());
+		
+		Computer comp2 = new Computer(500, 10, 24, 12, 8, "Lenovo", 2012);
+		Computer comp3 = new Computer(500, 10, 24, 12, 8, "Lenovo", 2012);
+		
+		System.out.println("Numero di PC al momento: " + comp.getPCnum());
 	}
 
 }
