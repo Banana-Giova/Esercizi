fbab29f1b3ad212c59092507f41df75d69fef819
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-10-21 13:52:14 +0200

    Progetto Java completato

---
+++ Java/P20241021_01_Agamennone/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double d;
+		d = Math.random();
+		
+		for (double i=1; i<=11; i++ ) {
+//			double d = Math.random();
+			System.out.println(d);
+		}
+	}
+
+}
