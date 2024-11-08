04708fb7725fb4bec11c02f1ee480a3ca77ff759
Banana-Giova <giovannidigiuseppe@protonmail.com>
2024-11-08 13:54:57 +0100

    Lezione 15 avanzata

--- Progettazione/Lezioni(B) da 11 a 20/Lezione15/Cielo4.sql
+++ Progettazione/Lezioni(B) da 11 a 20/Lezione15/Cielo4.sql
@@ -2,12 +2,37 @@
 --di tutti i voli della stessa compagnia? 
 --Restituire il codice del volo, la compagnia e la durata.
 
-
+WITH voli_comp AS (
+    SELECT AVG(durataMinuti) AS durmed,
+           comp AS compagnia
+    FROM volo
+    GROUP BY comp
+)
+SELECT volo.codice AS codice_del_volo,
+       volo.comp AS compagnia,
+       volo.durataMinuti AS durata
+FROM Volo volo
+    JOIN voli_comp vomp
+        ON vomp.compagnia = volo.comp
+WHERE volo.durataMinuti > vomp.durmed;
 
 --2. Quali sono le città che hanno piu” di un aeroporto 
 --e dove almeno uno di questi ha un volo operato da “Apitalia”?
 
-
+SELECT DISTINCT lurto.citta
+FROM LuogoAeroporto lurto
+    JOIN ArrPart part
+        ON part.partenza = lurto.aeroporto
+    JOIN ArrPart arr
+        ON arr.arrivo = lurto.aeroporto
+WHERE lurto.citta IN (
+	SELECT lurto1.citta
+    FROM LuogoAeroporto lurto1
+        JOIN Aeroporto aero1
+            ON lurto1.aeroporto = aero1.codice
+    GROUP BY lurto1.citta
+    HAVING COUNT(lurto1.citta) > 1
+    ) AND 
 
 --3. Quali sono le coppie di aeroporti (A, B) 
 --tali che esistono voli tra A e B ed il numero
