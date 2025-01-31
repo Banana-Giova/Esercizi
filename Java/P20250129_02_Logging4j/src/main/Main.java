7024a6baa72b7b3a7dfddf1fe6aa0e6d204a2251
Banana-Giova <giovannidigiuseppe@protonmail.com>
2025-01-31 19:15:47 +0100

    Progetti Java avanzato

--- Java/P20250129_02_Logging4j/src/main/Main.java
+++ Java/P20250129_02_Logging4j/src/main/Main.java
@@ -1,9 +1,13 @@
 package main;
+import models.Calcolatrice;
 
 public class Main {
 
 	public static void main(String[] args) {
-
+		Calcolatrice.somma(10, 20);
+		Calcolatrice.sottrazione(10, 20);
+		Calcolatrice.moltiplicazione(10, 20);
+		Calcolatrice.divisione(10, 20);
 	}
 
 }
--- Java/P20250129_02_Logging4j/src/models/Calcolatrice.java
+++ Java/P20250129_02_Logging4j/src/models/Calcolatrice.java
@@ -3,5 +3,29 @@ import org.apache.logging.log4j.*;
 
 public class Calcolatrice {
 	private static Logger log4jlogger = LogManager.getLogger(Calcolatrice.class.getName());
-
+	
+	public static int somma (int a, int b) {
+        log4jlogger.debug("Inizio operazione somma");
+        int risultato = a + b;
+        log4jlogger.info("Somma eseguita: {} + {} = {}", a, b, risultato);
+        return risultato;
+	}
+	public static int sottrazione (int a, int b) {
+		log4jlogger.debug("Inizio operazione sottrazione");
+        int risultato = a - b;
+        log4jlogger.info("Sottrazione eseguita: {} - {} = {}", a, b, risultato);
+        return risultato;
+	}
+	public static int moltiplicazione (int a, int b) {
+		log4jlogger.debug("Inizio operazione moltiplicazione");
+        int risultato = a * b;
+        log4jlogger.info("Moltiplicazione eseguita: {} x {} = {}", a, b, risultato);
+        return risultato;
+	}
+	public static double divisione (int a, int b) {
+		log4jlogger.debug("Inizio operazione divisione");
+        double risultato = (double)a/(double)b;
+        log4jlogger.info("Divisione eseguita: {} : {} = {}", a, b, risultato);
+        return risultato;
+	}
 }
