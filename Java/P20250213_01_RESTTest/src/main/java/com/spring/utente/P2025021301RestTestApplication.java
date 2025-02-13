ae32c0a218b94985f539bf8b674dea8ed9d66d9d
Banana-Giova <giovannidigiuseppe@protonmail.com>
2025-02-13 19:31:20 +0100

    Progetti Java avanzati

---
+++ Java/P20250213_01_RESTTest/src/main/java/com/spring/utente/P2025021301RestTestApplication.java
@@ -0,0 +1,13 @@
+package com.spring.utente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2025021301RestTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2025021301RestTestApplication.class, args);
+	}
+
+}
